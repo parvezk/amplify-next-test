91182413dee6ca0bcfbacaabea9c1ec69d90e02f
Parvez Kose <parvez.kosex@gmail.com>
2024-12-19 20:21:52 -0500

    added authenticator

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./app.css";
 
+import Authenticator from "./authenticator";
+
+import "@aws-amplify/ui-react/styles.css";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +20,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <Authenticator>{children}</Authenticator>
+      </body>
     </html>
   );
 }
